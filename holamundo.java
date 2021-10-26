75593396a22d1b92e0e83331fdf9f0d136d09875
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-10-26 17:29:45 -0500

    Create holamundo.java

---
+++ holamundo.java
@@ -0,0 +1,15 @@
+public class holamundo{
+	/**
+	*
+	*Aplicacion java
+	*Title:		Hola mundo en JAVA
+	*Descrition:	hola mundo en JAVA desde vim en windows
+	*Author:	Kevin Yoan Calderon Garcia
+	*Date		20211005
+	*Version:	1
+	*Usage:		C:\Users\Andrea\Documents\Java
+	*/
+		public static void main(String []args){
+			System.out.println("HOLA MUNDO");//prints Hello World
+	}
+}
